481ce52f35e8dd6be9c2e94030a8da3ccc89b428
Kartavya kothari <kartavya.kothari24@gmail.com>
2018-01-25 13:40:24 +0530

    Made selection sort more modular and structured

--- sorting/selectionsort.c
+++ sorting/selectionsort.c
@@ -1,16 +1,10 @@
 #include<stdio.h>
 
-int main(int argc, char const *argv[]) {
-    int arr[] = {710,841,491,833,740,491,502,251,864,377,719,682,245,425,851,961,69,990,290,265};
-    int min,index;
-
-    for(int i = 0 ; i < 20 ; i++)
-        printf("%d ", arr[i]);
-
-    for(int i = 0 ; i < 20 ; i++){
+void selectionsort(int arr[],int n){
+    for(int i = 0 ; i < n ; i++){
         min = arr[i];
         index = i;
-        for(int j = i + 1 ; j < 20 ; j++){
+        for(int j = i + 1 ; j < n ; j++){
             if(arr[j]<min){
                 min = arr[j];
                 index = j;
@@ -20,6 +14,16 @@ int main(int argc, char const *argv[]) {
         arr[i] = min;
         arr[index] = temp;
     }
+}
+
+int main(int argc, char const *argv[]) {
+    int arr[] = {710,841,491,833,740,491,502,251,864,377,719,682,245,425,851,961,69,990,290,265};
+    int min,index;
+
+    for(int i = 0 ; i < 20 ; i++)
+        printf("%d ", arr[i]);
+
+    selectionsort();
 
     putchar('\n');
 
