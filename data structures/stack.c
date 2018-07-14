925b95c70aee451d9cc306809128eb9101e746c7
Kartavya kothari <kartavya.kothari24@gmail.com>
2018-07-14 15:18:08 +0530

    Added stack

---
+++ data structures/stack.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+
+#define SIZE 100
+
+int stack[SIZE];
+int stackTop = -1;
+
+void push(int ele){
+    if(stackTop = SIZE -1){
+        printf("Stack overflow\n");
+        return;
+    }
+    stack[++stackTop] = ele;
+}
+
+void pop(){
+    if(stackTop==-1){
+        printf("Stack Empty\n");
+        return;
+    }
+    printf("%d", stack[stackTop--]);
+    // return stack[stackTop--];
+}
+
+void displayStack(){
+    for(int i = 0 ; i < stackTop ; i++){
+        printf("%d ",stack[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char const *argv[])
+{
+    int res;
+    int ele;
+    int isTrue = 1;
+
+    while(isTrue){
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Display\n");
+        printf("4. Stop\n");
+
+        scanf("%d",&res);
+
+        switch(res){
+            case 1:
+                printf("Enter element: ");
+                scanf("%d",&ele);    
+                push(ele);
+                break;
+            case 2: 
+                pop();
+                break;
+            case 3:
+                displayStack();
+                break;
+            case 4:
+                isTrue=0;
+                break;    
+        }
+    }
+    return 0;
+}
